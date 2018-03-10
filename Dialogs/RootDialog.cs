020df07651da8bcfd28922fdc29370367bad1914
AJ <acjones@protonmail.com>
2018-03-10 12:24:08 +0000

    Initial bot

---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        protected int Count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync($"Welcome {context.Activity.From.Name}");
+            
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            switch (message.Text)
+            {
+                case "reset":
+                    PromptDialog.Confirm(
+                        context,
+                        AfterResetAsync,
+                        "Are you sure you want to reset the count?",
+                        "Didn't get that!",
+                        promptStyle: PromptStyle.Auto);
+                    break;
+                case "talk about something more intresting":
+                    PromptDialog.Choice(context, AfterMoreIntrestingAsync, new List<string> {"a", "b", "c"}, "ok how about the alphabet?");
+                    break;
+                default:
+                    await context.PostAsync($"{Count++}: You said {message.Text}");
+                    context.Wait(MessageReceivedAsync);
+                    break;
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.Count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task AfterMoreIntrestingAsync(IDialogContext context, IAwaitable<string> argument)
+        {
+            var choice = await argument;
+            switch (choice)
+            {
+                case "a":
+                    await context.PostAsync("a");
+                    break;
+                case "b":
+                    await context.PostAsync("b");
+                    break;
+                case "c":
+                    await context.PostAsync("c");
+                    break;
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbot102-Web-Deploy.pubxml -p:Password=gAsusZPLtibklpMeTlWAPean9xwAWSQFnPBer6pCbcrmAeWk00p5QCwWsR1g
+
